5c350101a9bfee9c701b85542e30a5e40ac9cc95
Sanskargupta0 <sanskar362002@gmail.com>
2024-11-13 00:48:18 +0530

    Added: Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import { Nav } from "./components";
+import { Hero } from "./sections"
 
 const App = () => {
   return (
     <main className='relative'>
       <Nav />
+      <section className='xl:padding-l wide:padding-r padding-b'>
+        <Hero />
+      </section>
     </main>
   );
 };
